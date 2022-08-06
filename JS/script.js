6d7305487fd3d0498d744dbb384863f18a17a91c
Leksip <Nickita.rom@gmail.com>
2022-08-06 12:19:57 +0300

    переписал переменные, получил элементы и перебрал их ч\з forEach

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,10 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const input = document.querySelector('.calc__input');
+  const input = document.querySelector('input');
 // цифры
-  const num1 = document.querySelector('.calc__button_1');
-  const num2 = document.querySelector('.calc__button_2');
-  const num3 = document.querySelector('.calc__button_3');
-  const num4 = document.querySelector('.calc__button_4');
-  const num5 = document.querySelector('.calc__button_5');
-  const num6 = document.querySelector('.calc__button_6');
-  const num7 = document.querySelector('.calc__button_7');
-  const num8 = document.querySelector('.calc__button_8');
-  const num9 = document.querySelector('.calc__button_9');
+  const numbers = document.querySelectorAll('.calc__button_num');
+
   // действия
   const division = document.querySelector('[data-action="division"]');
   const multiply = document.querySelector('[data-action="multiply"]');
@@ -24,36 +17,39 @@ window.addEventListener('DOMContentLoaded', () => {
   const remainder = document.querySelector('[data-action="remainder"]');
 
   let arr = [];
-  let string = '';
+  let number1 = '';
+  let number2 = '';
+  let action = '';
   let result = 0;
+  const getSubtracting = '';
 
-  function strPush(button, num) {
-    button.addEventListener('click', () => {
-      string += num;
-      input.innerHTML = '';
-      input.innerHTML = `<input class="calc__field" type="number" placeholder = ${string}>`;
-      // console.log(string);
 
+// добавляем цифры
+  numbers.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      if (action === '') {
+        number1 += e.target.value;
+      } else {
+        number2 += e.target.value;
+      }
+      input.value += e.target.value;
+      console.log()
     });
-  };
-  strPush(num1, 1);
-  strPush(num2, 2);
-  strPush(num3, 3);
-  strPush(num4, 4);
-  strPush(num5, 5);
-  strPush(num6, 6);
-  strPush(num7, 7);
-  strPush(num8, 8);
-  strPush(num9, 9);
+
+  });
+
 
 // очистить строку
-  clear.addEventListener('click', () =>{
-    input.innerHTML = '<input class="calc__field" type="number" placeholder = "0">';
-    string = '';
-    console.log(string);
-  })
+  reset.addEventListener('click', () => {
+    input.value = 0;
+    number1 = '';
+    number2 = '';
+    console.log(number1);
+  });
+  subtracting.addEventListener('click', () => {
+    input.value = 0;
+    input.innerHTML = `<input class="calc__field" type="number" placeholder = ${string}->`;
+  });
+  console.log(input.value);
 
-  console.log(input);
-
-})
-;
+});
